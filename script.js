235d6f60d87d449c69842e8d4cf325a4741ec037
moiz <pc16786abdulmoiz@gmail.com>
2024-07-12 21:39:39 +0500

    add loader , logo , and set navbar

--- script.js
+++ script.js
@@ -15,10 +15,42 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+// scroll 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.getElementById('Navbar');
+  
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 0) {
+        header.classList.add('sticky');
+        header.classList.remove('slow-transition');
+      } else {
+        header.classList.remove('sticky');
+        header.classList.add('slow-transition');
+      }
+    });
+  
+    // Adjust scroll position on page load, in case user loads the page already scrolled
+    if (window.scrollY > 0) {
+      header.classList.add('sticky');
+    }
+  });
+  
+
 
 // loader 
-
-function myLoader() {
+document.addEventListener("DOMContentLoaded", function() {
+    // Show the loader when the DOM is fully loaded
     let preloader = document.getElementById("loader");
-    preloader.style.display = "none";
-}
+    preloader.style.display = "block";
+});
+
+window.addEventListener("load", function() {
+    // Ensure the loader is shown for at least 2 seconds
+    let preloader = document.getElementById("loader");
+    let mainContent = document.getElementById("main-content");
+    setTimeout(function() {
+        preloader.style.display = "none";
+        mainContent.style.display = "block";
+    }, 1000); // 2 seconds
+});
